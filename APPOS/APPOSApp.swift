602f20aa52fb419e9c059b27f46835a068f7e3f6
Jay <jexwang@icloud.com>
2020-12-06 16:37:19 +0800

    Initial Commit

---
+++ APPOS/APPOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  APPOSApp.swift
+//  APPOS
+//
+//  Created by 王冠綸 on 2020/12/6.
+//
+
+import SwiftUI
+
+@main
+struct APPOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APPOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  APPOS
+//
+//  Created by 王冠綸 on 2020/12/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
